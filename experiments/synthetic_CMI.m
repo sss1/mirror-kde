d5fb42d3cad610b0dded9a0235eede9d73521084
sss1 <sss1@andrew.cmu.edu>
2014-10-02 14:23:33 -0400

    Multidimensional version working.

--- experiments/synthetic_CMI.m
+++ experiments/synthetic_CMI.m
@@ -1,11 +1,11 @@
 % experiment parameters
 alpha = 2;
-n_trials = 24;
-ns = [500 700 1000 1500 2000 2500 3000 5000 8000 10000];
+n_trials = 12;
+ns = [500 700 1000 1500 2000 2500 3000 5000 8000]; % 10000];
 ns = 24*(floor(ns/24)); % round sample sizes to simplify data splitting
-hs = [0.1 0.05 0.01];
-kernel = @(x) max((1 - x^2)*3/4, 0); % Epanechnikov kernel
-% kernel = @(x) normpdf(x); % Gaussian kernel
+hs = [0.1 0.01];
+% kernel = @(x) max((1 - x^2)*3/4, 0); % Epanechnikov kernel
+kernel = @(x) normpdf(x); % Gaussian kernel
 dim = 3;
 
 % allocate space for results
@@ -26,19 +26,19 @@ for ni=1:length(ns) % for each sample size
       Xs = normrnd(Zs, 1);
       Z2s = normrnd(0, 1, n, dim);
 
-      if trial <= 2
+      if trial <= 1
         tic
       end
       % compute estimates of CMI and MI
-      CMIs(trial,ni,hi) = CMI_est(kernel, h, alpha, Xs, Ys, Zs)
+      CMIs(trial,ni,hi) = CMI_est(kernel, h, alpha, Xs, Ys, Zs);
       MIs(trial,ni,hi) = CMI_est(kernel, h, alpha, Xs, Ys, Z2s);
-      if trial <= 2
+      if trial <= 1
         toc
       end
     end
     [ni hi] % report completed (sample size, bandwidth) pair
 
     % save results and parameters
-    save('multivar_3d.mat','CMIs','MIs','ns','hs','alpha','kernel','n_trials');
   end
+  save('multivar_3d2.mat','CMIs','MIs','ns','hs','alpha','kernel','n_trials');
 end
